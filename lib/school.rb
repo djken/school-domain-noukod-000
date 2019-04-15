d327145482df66e9eaef6e45e98d2f28380a79be
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-15 13:56:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,11 +19,11 @@ class School
   end
 end
 
-  # def sort
+  def sort
   #   sort_hash = {}
   #   roster.each {|key, student|} sort_hash[key] = student.sort
   #   sort_hash
-  # end
+  end
 
 end
 
